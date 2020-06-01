d255adf3bc1219731e790da0597672d953f47d9e
Mentat_Radnor <kholosinc@gmail.com>
2020-06-01 10:25:08 +0300

    Module 2 complete

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,7 +1,6 @@
-/**
- * Проверяем объект obj на пустоту
- * @param {Object} obj
- * @returns {Boolean}
- */
 function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  };
+  return true
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
+
+read(a,b) {
+this.a = a || +prompt (`Введите число`);
+this.b = b || +prompt (`Введите число`);
+},
+
+ sum() {
+   return this.a + this.b
+ },
+
+ mul(){
+   return this.a * this.b
+ },
 };
+calculator.read(3,2);
+alert( calculator.sum() );
+alert( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
