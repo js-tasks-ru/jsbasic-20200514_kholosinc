a8d3568b50dbe54b9666bed62356a18ba67e9acb
Mentat_Radnor <kholosinc@gmail.com>
2020-10-11 14:47:49 +0300

    complete module 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,12 @@
-/**
- * @param {HTMLTableElement} table
- * @return {void}
- */
 function makeDiagonalRed(table) {
+  let rows = table.rows;
+  for (let row of rows) {
+    let i = row.sectionRowIndex;
+    row.cells[i].style.background = "red"
+  }
+  // let tr = document.querySelectorAll("tr");
+  // for (let i = 0; i < tr.length; i++) {
+  //   let td = tr[i].querySelectorAll("td")
+  //   td[i].style.background = "red"
+  // }
 }
