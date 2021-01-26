15205f670cd8d98452192415961e1fba1e1f45dc
Mentat_Radnor <kholosinc@gmail.com>
2021-01-26 20:59:57 +0300

    Урок 5

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -4,9 +4,4 @@ function makeDiagonalRed(table) {
     let i = row.sectionRowIndex;
     row.cells[i].style.background = "red"
   }
-  // let tr = document.querySelectorAll("tr");
-  // for (let i = 0; i < tr.length; i++) {
-  //   let td = tr[i].querySelectorAll("td")
-  //   td[i].style.background = "red"
-  // }
 }
--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,6 +1,24 @@
-/**
- * Метод устанавливает необходимые по условию аттрибуты таблице
- * @param {Element} table
- */
 function highlight(table) {
+  let tbody = table.tBodies;
+  let rows = tbody[0].rows;
+  for (let row of rows) {
+    let status = row.cells[3];
+    let gender = row.cells[2];
+    let age = row.cells[1];
+    if (status.dataset.available == "true") {
+      row.classList.add("available")
+    } else if (status.dataset.available == "false") {
+      row.classList.add("unavailable")
+    } else {
+      row.setAttribute("hidden", "hidden")
+    }
+    if (gender.innerHTML == "m") {
+      row.classList.add("male");
+    } else {
+      row.classList.add("female");
+    }
+    if (age.innerHTML < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
